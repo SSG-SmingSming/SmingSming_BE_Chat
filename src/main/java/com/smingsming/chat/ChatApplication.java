5b88aadff06bb7392a12ce8b1eaff91e96e95297
Bear-Sik <108394795+Bear-Sik@users.noreply.github.com>
2022-11-10 01:14:11 +0900

    Update Timezone

--- src/main/java/com/smingsming/chat/ChatApplication.java
+++ src/main/java/com/smingsming/chat/ChatApplication.java
@@ -5,11 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 public class ChatApplication {
+
+    @PostConstruct
+    public void timeZoneInit() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ChatApplication.class, args);
